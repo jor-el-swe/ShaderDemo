070fcacf267d0c6fcd16c82a9a9422a56fc2b373
Joel Ring <9348156+jor-el-swe@users.noreply.github.com>
2021-07-06 15:48:37 +0200

    implements some kind of AI run and jump

--- Assets/scenes/autoplay.gd
+++ Assets/scenes/autoplay.gd
@@ -1,14 +1,32 @@
 extends Node2D
 
+var isMovingGuy = false
+
+
+var evRight = InputEventAction.new()
+var evJump = InputEventAction.new()
 
 func _ready():
+	evRight.action = "right"
+	evJump.action = "jump"
 	pass
 	
 func _physics_process(delta):
-	var ev = InputEventAction.new()
-	# Set as move_left, pressed.
-	ev.action = "right"
-	ev.pressed = true
-	# Feedback.
-	Input.parse_input_event(ev)
+	
+
+
+	if $floorChecker.is_colliding():
+		evJump.pressed = false
+		Input.parse_input_event(evJump)
+		if !isMovingGuy:
+			print("moves once")
+			isMovingGuy = true
+			evRight.pressed = true
+			Input.parse_input_event(evRight)
+			
+	if not$floorChecker.is_colliding() and get_parent().is_on_floor():
+		print("no ground!")
+		
+		evJump.pressed = true
+		Input.parse_input_event(evJump)
 
